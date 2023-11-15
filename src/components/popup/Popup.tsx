bf48e764d3d64134fa4a1768af3775d02aea4e03
Ash Bars <barsegyan@topsystems.ru>
2023-11-15 16:54:26 +0300

    -добавить анимацию  transition: grid-template-columns

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -4,33 +4,35 @@ import './Popup.scss';
 
 export const Popup = (props: any) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [isSidebarHidden, setIsSidebarHidden] = useState<boolean>(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
 
   const openModal = (e:  React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     console.log('open modal')
     setIsOpen(true)
   };
   const toggleSidebar = () => {
-    setIsSidebarHidden(!isSidebarHidden);
+    console.log('change sidebar visibility', isSidebarOpen)
+    setIsSidebarOpen(!isSidebarOpen);
   };
   return (
     <div className={'modal-test'}>
       <button onClick={openModal}>open modal</button>
       <div id="myModal" className={`modal ${isOpen ? 'modal--open' : ''}`}>
-        <div className={`modal-content ${isSidebarHidden ? 'sidebar-hidden' : ''}`}>
-          <div className="modal-content__sidebar">
-            <ol>
-              <li>1title</li>
-              <li>2title</li>
-              <li>3title</li>
-              <li>4title</li>
-              <li>5title</li>
-            </ol>
+        <div className={`modal-content`}>
+          <div className={`modal-content__sidebar ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+            <div className={'groups-flex'}>
+              <a>1title </a>
+              <a>2title </a>
+              <a>3title </a>
+              <a>4title as </a>
+              <a>5title </a>
+            </div>
+            <button className={`open-bar-button`} onClick={toggleSidebar}>+</button>
           </div>
           <div className="modal-content__page-content">
             some info
             <div className="page-content__close-button" onClick={(e) => setIsOpen(false)}>&times;</div>
-            <button className={'tab-menu-switcher'} onClick={toggleSidebar}>&#8617;</button>
+
           </div>
         </div>
       </div>
