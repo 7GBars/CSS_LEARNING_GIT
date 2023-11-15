93eb6804c4a0cb4c1a780266da48ddef1f558f83
Ash Bars <barsegyan@topsystems.ru>
2023-11-15 15:51:59 +0300

    -добавить анимацию закрытия sidebar

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -4,17 +4,20 @@ import './Popup.scss';
 
 export const Popup = (props: any) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isSidebarHidden, setIsSidebarHidden] = useState<boolean>(false);
 
   const openModal = (e:  React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     console.log('open modal')
     setIsOpen(true)
   };
-
+  const toggleSidebar = () => {
+    setIsSidebarHidden(!isSidebarHidden);
+  };
   return (
     <div className={'modal-test'}>
       <button onClick={openModal}>open modal</button>
       <div id="myModal" className={`modal ${isOpen ? 'modal--open' : ''}`}>
-        <div className="modal-content">
+        <div className={`modal-content ${isSidebarHidden ? 'sidebar-hidden' : ''}`}>
           <div className="modal-content__sidebar">
             <ol>
               <li>1title</li>
@@ -27,7 +30,7 @@ export const Popup = (props: any) => {
           <div className="modal-content__page-content">
             some info
             <div className="page-content__close-button" onClick={(e) => setIsOpen(false)}>&times;</div>
-            <button className={'tab-menu-switcher'} onClick={(e) => {/* тут код для сокрытия .modal-content__sidebar*/}}>&#8617;</button>
+            <button className={'tab-menu-switcher'} onClick={toggleSidebar}>&#8617;</button>
           </div>
         </div>
       </div>
