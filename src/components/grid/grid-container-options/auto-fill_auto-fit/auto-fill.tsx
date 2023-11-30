15526ebf624df80d475da1a1679470f803f5e4b2
Ash Bars <barsegyan@topsystems.ru>
2023-11-30 20:33:15 +0300

    -добавить иконки fontawesome -изменить цвет и размер иконки(наследуются)

--- src/components/grid/grid-container-options/auto-fill_auto-fit/auto-fill.tsx
+++ src/components/grid/grid-container-options/auto-fill_auto-fit/auto-fill.tsx
@@ -5,8 +5,15 @@ import './auto-fill.scss';
 export const AutoFill = (props:  any) => {
   return (
     <div className={'grid-container1'}>
-      <div className="item1">test</div>
-      <div className="item1">Lorem ipsum dolor sit amet.</div>
+      <div className="item1">test
+        <i className="fa-solid fa-user"></i>
+      </div>
+      <div className="item1">
+        Lorem ipsum dolor sit amet.
+        <span style={{fontSize: '3em', color: 'Tomato'}}>
+          <i className="fa-solid fa-camera"></i>
+        </span>
+      </div>
       <div className="item1"></div>
       <div className="item1">Lorem ipsum dolor</div>
       <div className="item1"> ipsum dolor</div>
